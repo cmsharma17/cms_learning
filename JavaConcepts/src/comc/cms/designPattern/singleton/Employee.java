d75ee85fae7a7bf8476bc25db3041509c001afcd
chandramoulisharma <cmsharma.cs@gmail.com>
2019-12-01 12:47:13 +0530

    check double

--- JavaConcepts/src/comc/cms/designPattern/singleton/Employee.java
+++ JavaConcepts/src/comc/cms/designPattern/singleton/Employee.java
@@ -9,6 +9,7 @@ final class Employee {
 	public final static Employee getInstance(){
 		if(obj == null){
 			synchronized(Employee.class){
+				//Double Check
 				if(obj == null){
 					obj =  new Employee();
 				}
